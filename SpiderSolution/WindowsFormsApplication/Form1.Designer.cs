4c9881b233e08ec6c51c8e6de23272bffd53a450
Kael's PC <kaelkong@me.com>
2017-03-10 18:09:37 +0800

    界面底层架构

--- SpiderSolution/WindowsFormsApplication/Form1.Designer.cs
+++ SpiderSolution/WindowsFormsApplication/Form1.Designer.cs
@@ -28,64 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSourceFile = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbSourceFile = new System.Windows.Forms.TextBox();
+            this.tbSougou = new System.Windows.Forms.TextBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.lbSourceFile = new System.Windows.Forms.Label();
+            this.lbSougou = new System.Windows.Forms.Label();
+            this.openSourceFile = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSourceFile
             // 
-            this.button1.Location = new System.Drawing.Point(1428, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(316, 73);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSourceFile.Location = new System.Drawing.Point(714, 6);
+            this.btnSourceFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSourceFile.Name = "btnSourceFile";
+            this.btnSourceFile.Size = new System.Drawing.Size(158, 36);
+            this.btnSourceFile.TabIndex = 0;
+            this.btnSourceFile.Text = "源文件";
+            this.btnSourceFile.UseVisualStyleBackColor = true;
+            this.btnSourceFile.Click += new System.EventHandler(this.btnSourceFile_Click);
             // 
-            // button2
+            // btnStart
             // 
-            this.button2.Location = new System.Drawing.Point(1428, 91);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(316, 73);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnStart.Location = new System.Drawing.Point(714, 46);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(158, 36);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // textBox1
+            // tbSourceFile
             // 
-            this.textBox1.Location = new System.Drawing.Point(217, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1169, 35);
-            this.textBox1.TabIndex = 1;
+            this.tbSourceFile.Location = new System.Drawing.Point(108, 16);
+            this.tbSourceFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbSourceFile.Name = "tbSourceFile";
+            this.tbSourceFile.Size = new System.Drawing.Size(586, 21);
+            this.tbSourceFile.TabIndex = 1;
             // 
-            // textBox2
+            // tbSougou
             // 
-            this.textBox2.Location = new System.Drawing.Point(217, 112);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(1169, 35);
-            this.textBox2.TabIndex = 1;
+            this.tbSougou.Location = new System.Drawing.Point(108, 56);
+            this.tbSougou.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbSougou.Name = "tbSougou";
+            this.tbSougou.Size = new System.Drawing.Size(586, 21);
+            this.tbSougou.TabIndex = 1;
             // 
             // webBrowser1
             // 
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.webBrowser1.Location = new System.Drawing.Point(0, 170);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Location = new System.Drawing.Point(0, 86);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(10, 10);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1845, 670);
+            this.webBrowser1.Size = new System.Drawing.Size(878, 665);
             this.webBrowser1.TabIndex = 2;
+            // 
+            // lbSourceFile
+            // 
+            this.lbSourceFile.AutoSize = true;
+            this.lbSourceFile.Location = new System.Drawing.Point(12, 19);
+            this.lbSourceFile.Name = "lbSourceFile";
+            this.lbSourceFile.Size = new System.Drawing.Size(77, 12);
+            this.lbSourceFile.TabIndex = 3;
+            this.lbSourceFile.Text = "源文件路径：";
+            // 
+            // lbSougou
+            // 
+            this.lbSougou.AutoSize = true;
+            this.lbSougou.Location = new System.Drawing.Point(12, 59);
+            this.lbSougou.Name = "lbSougou";
+            this.lbSougou.Size = new System.Drawing.Size(65, 12);
+            this.lbSougou.TabIndex = 3;
+            this.lbSougou.Text = "抓取地址：";
+            // 
+            // openSourceFile
+            // 
+            this.openSourceFile.FileName = "openSourceFile";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1845, 840);
+            this.ClientSize = new System.Drawing.Size(878, 751);
+            this.Controls.Add(this.lbSougou);
+            this.Controls.Add(this.lbSourceFile);
             this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSougou);
+            this.Controls.Add(this.tbSourceFile);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSourceFile);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -95,11 +130,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSourceFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbSourceFile;
+        private System.Windows.Forms.TextBox tbSougou;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label lbSourceFile;
+        private System.Windows.Forms.Label lbSougou;
+        private System.Windows.Forms.OpenFileDialog openSourceFile;
     }
 }
 
--- SpiderSolution/WindowsFormsApplication/Form1.cs
+++ SpiderSolution/WindowsFormsApplication/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,33 @@ namespace WindowsFormsApplication
 {
     public partial class Form1 : Form
     {
+        private string ConnStr = @"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = {0}; Extended Properties = 'Excel 8.0;HDR=Yes;IMEX=1;'";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSourceFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openSourceFile.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                tbSourceFile.Text = openSourceFile.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string connStr = string.Format(ConnStr, tbSourceFile.Text.Trim());
+            OleDbConnection conn = new OleDbConnection(connStr);
+            conn.Open();
+            using (OleDbCommand cmd = conn.CreateCommand())
+            {
+                DataTable schemaTable = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new Object[] { null, null, null, "TABLE" });
+
+                cmd.CommandText = "SELECT * FROM [" + schemaTable.Rows[0]["TABLE_NAME"].ToString() + "]";
+            }
         }
     }
 }
