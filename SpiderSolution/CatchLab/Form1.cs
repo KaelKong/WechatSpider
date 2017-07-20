1b4948cd14edf97b6138fc2e3ab3fa7be53e7ab0
Kael's PC <kaelkong@me.com>
2017-07-20 15:10:25 +0800

    添加测试程序 CatchLab

---
+++ SpiderSolution/CatchLab/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MjM5NjcwNzA2Mw==&scene=124#wechat_redirect");
+        }
+    }
+}
